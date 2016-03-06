99251a16fe680b7d37c926ac755c4d59774addb4
Howard Jones <howie@thingy.com>
2016-03-06 11:33:10 +0000

    Interval part mostly works, image updating doesn't.

--- dashboard.js
+++ dashboard.js
@@ -1,5 +1,31 @@
 function refresh_handler() {
-  console.log("Refresh!");
+  var d = new Date();
+  var append = "?" + d.getTime();
+  var url = $(this).data('url');
+  var my_type = $(this).prop('tagName');
+  if (my_type == 'IMG') {
+    url = $(this).attr('src');
+  }
+
+  console.log("Refresh " + my_type + " from " + url);
+
+  if (url) {
+    $.get({url: url + append, success: function(data) {
+        console.log("GOT");
+
+        if (my_type == 'IMG') {
+          // change SRC
+          console.log("It's an image, so set the src");
+          $(this).src = data;
+
+        } else {
+          // change html()
+          console.log("It's HTML, so set the content");
+          $(this).html(data);
+        }
+
+    }});
+  }
 }
 
 $(document).ready(function() {
@@ -7,8 +33,13 @@ $(document).ready(function() {
     console.log("found reloadable: " + $(this).attr('id'));
     var period = parseInt($(this).data('refresh'));
     var url = $(this).data('url');
+    var my_type = $(this).prop('tagName');
 
-    console.log(period + "/" + url);
+    if (my_type == 'IMG') {
+      url = $(this).attr('src');
+    }
+
+    console.log(period + "/" + my_type + "/" + url);
 
     if (period > 0 && url) {
       console.log("Added handler");
