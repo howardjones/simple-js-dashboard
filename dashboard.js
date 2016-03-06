fb6607189a6074a582bb1ed7cbbb9c5595b53920
howardjones <howie@thingy.com>
2016-03-06 12:58:56 +0000

    missed the actual code

--- dashboard.js
+++ dashboard.js
@@ -3,47 +3,56 @@ function refresh_handler() {
   var append = "?" + d.getTime();
   var url = $(this).data('url');
   var my_type = $(this).prop('tagName');
-  if (my_type == 'IMG') {
-    url = $(this).attr('src');
-  }
 
-  console.log("Refresh " + my_type + " from " + url);
+  // console.log("Refresh " + my_type + " from " + url);
+
+  var that = this;
+  var id = $(this).attr('id');
 
   if (url) {
-    $.get({url: url + append, success: function(data) {
-        console.log("GOT");
-
-        if (my_type == 'IMG') {
-          // change SRC
-          console.log("It's an image, so set the src");
-          $(this).src = data;
-
-        } else {
-          // change html()
-          console.log("It's HTML, so set the content");
-          $(this).html(data);
-        }
-
-    }});
+    if (my_type == 'IMG') {
+      var loader = $('#loader_' + id);
+      loader.attr('src', url + append);
+    //  console.log(loader);
+    } else {
+      $.get({url: url + append, success: function(data) {
+      //    console.log("GOT");
+          $(that).html(data);
+        }});
+    }
   }
 }
 
 $(document).ready(function() {
+
+  $('body').append("<div id='dashboard_loaders' style='display:none'></div>")
+
   $('.reloadable').each( function() {
-    console.log("found reloadable: " + $(this).attr('id'));
+    //console.log("found reloadable: " + $(this).attr('id'));
     var period = parseInt($(this).data('refresh'));
     var url = $(this).data('url');
     var my_type = $(this).prop('tagName');
+    var id = $(this).attr('id');
 
     if (my_type == 'IMG') {
       url = $(this).attr('src');
+      $(this).data('url', url);
+      // for each image, there's a hidden img tag to load in the background
+      $('#dashboard_loaders').append("<img class='reloadable_loader' id='loader_" + id + "' data-url='" + url + "' />")
     }
 
-    console.log(period + "/" + my_type + "/" + url);
+    // console.log(period + "/" + my_type + "/" + url);
 
     if (period > 0 && url) {
-      console.log("Added handler");
       $(this).everyTime(period*1000,refresh_handler,$(this).attr('id'));
     }
+
+    $('.reloadable_loader').load( function() {
+      var id = $(this).attr('id');
+      var owner = id.substr(7);
+      $('#' + owner).attr('src', $(this).attr('src'));
+    });
+
+
   });
 });
